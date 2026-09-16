f511889bdc19d52b0cb8f999022b40a77a2e0437
lcsilvaprof <prof.silva76@gmail.com>
2026-09-16 07:39:21 -0300

    ajustes no titulo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-blue-600 font-bold">Exemplo Git Flow</h1>
+      <h1 className="text-3xl text-blue-600 font-bold">Exemplo Git Flow - testes</h1>
     </>
   )
 }
